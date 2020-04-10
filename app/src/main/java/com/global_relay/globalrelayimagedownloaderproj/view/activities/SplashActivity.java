f73194cbb94577657424c6876da370f4078cda2e
Farshad <fzcomputerscience@gmail.com>
2020-04-09 23:26:34 -0400

    Splash activity has updated

--- app/src/main/java/com/global_relay/globalrelayimagedownloaderproj/view/activities/SplashActivity.java
+++ app/src/main/java/com/global_relay/globalrelayimagedownloaderproj/view/activities/SplashActivity.java
@@ -21,7 +21,6 @@ public class SplashActivity extends AppCompatActivity
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
         checkRequirePermissions();
     }
     @Override
